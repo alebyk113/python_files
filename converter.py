ba02206bb01d7e2f2e853cc01600d57f4ce7dfba
Ola Bykowska <ab14235@bristol.ac.uk>
2016-09-26 13:01:04 +0100

    converts files into csv

---
+++ converter.py
@@ -0,0 +1,15 @@
+import os
+#import glob
+
+#print glob.glob("/desktop/*.docx")
+
+def convert(file_list):
+    file_list=file_list
+    for i in file_list:
+        base = os.path.splitext(i)[0]
+        os.rename(i, base + ".csv")
+    
+    
+file_list=["66140156.AP0"] #,"65190095.AP0"]#,"63300548.AP0"]#,"63080109.AP0","63080119.AP0","63080144.AP0","63080156.AP0"]
+#,"65310052.AP0","65310054.AP0","65310056.AP0","6531058.AP0","63080156.AP0","63080076.AP0",]
+convert(file_list)
