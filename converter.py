965394ff498a381df3f7ecfd695dd99dadc598d4
Ola Bykowska <ab14235@bristol.ac.uk>
2016-12-05 10:47:44 +0000

    aa

--- converter.py
+++ converter.py
@@ -1,6 +1,9 @@
 import os
 #import glob
 
+
+#change it so i can convert whole folders
+
 #print glob.glob("/desktop/*.docx")
 
 def convert(file_list):
@@ -10,6 +13,6 @@ def convert(file_list):
         os.rename(i, base + ".csv")
         print (i)
     
-file_list=["63290355.AT0","63290348.AT0","63290341.AT0"] #,"65190095.AP0"]#,"63300548.AP0"]#,"63080109.AP0","63080119.AP0","63080144.AP0","63080156.AP0"]
+file_list=["6N170151.P0"] #,"6N170151.P0"] #,"63290341.AT0"] #,"65190095.AP0"]#,"63300548.AP0"]#,"63080109.AP0","63080119.AP0","63080144.AP0","63080156.AP0"]
 #,"65310052.AP0","65310054.AP0","65310056.AP0","6531058.AP0","63080156.AP0","63080076.AP0",]
 convert(file_list)
